b70cb0ddbf145e55e090b5b597349d55c1d2357d
CORP\christopher.demicoli <christopher.demicoli@gaminginnovationgroup.com>
2017-01-15 14:09:14 +0100

    Connected to the IMAP Server

--- IMAPPayslipDownloader/Program.cs
+++ IMAPPayslipDownloader/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using S22.Imap;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,11 @@ namespace IMAPPayslipDownloader
     {
         static void Main(string[] args)
         {
+            var settings = JsonConvert.DeserializeObject<SettingsModel>(File.ReadAllText("settings.json"));
+            using (ImapClient Client = new ImapClient(settings.Host, settings.Port, settings.Username, settings.Password, ssl: settings.SSL))
+            {
+                Console.WriteLine("We are connected!");
+            }
         }
     }
 }
--- IMAPPayslipDownloader/SettingsModel.cs
+++ IMAPPayslipDownloader/SettingsModel.cs
@@ -1,7 +1,15 @@
-﻿namespace IMAPPayslipDownloader
+﻿using System;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace IMAPPayslipDownloader
 {
     public class SettingsModel
     {
         public string Host { get; set; }
+        public int Port { get; set; }
+        public bool SSL { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
